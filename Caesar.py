a0a0ea535d290ef6b07790252b355421818c6fe4
besele07 <besele07@rg16.at>
2023-10-02 16:01:03 +0200

    corrected some errors added random msg text case

--- Caesar.py
+++ Caesar.py
@@ -3,17 +3,22 @@ abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"
 
 def encrypt(msg_clear: str, key: int):
     msg_hidden = ""
+
     for c in msg_clear.lower():
         try:
             i = abc.index(c)
-            new_i = i + key
+            new_i = (i + key) % 26
             new_c = abc[new_i]
         except ValueError:
-            new_c = c
+            if c.isnumeric():
+                i = int(c)
+                new_i = (i + key) % 10
+                new_c = str(new_i)
+            else:
+                new_c = c
 
         msg_hidden += new_c
     return msg_hidden
 
-print(encrypt("hai", 1))
-print(encrypt("wo", 2))
-print(encrypt("fff", 3))
+def decrypt(hidden_msg: str, key: int):
+    return encrypt(hidden_msg, -key)
--- test_Caesar.py
+++ test_Caesar.py
@@ -1,5 +1,25 @@
 import pytest
 import Caesar
+import random
+import string
+
+
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.printable
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+def test_encrypt_random():
+    for i in range(1000):
+        clear = get_random_string(random.randint(0, 100))
+        key = random.randint(0, 100)
+        hidden = Caesar.encrypt(clear, key)
+        decrypted = Caesar.decrypt(hidden, key)
+        assert clear.lower() == decrypted
+        print(clear)
+
 
 def test_encrypt_basic():
     assert Caesar.encrypt("abc", 1) == "bcd"
@@ -8,4 +28,5 @@ def test_encrypt_basic():
     assert Caesar.encrypt("hallo", 2) == "jcnnq"
     assert Caesar.encrypt("Hi", 2) == "jk"
     assert Caesar.encrypt("Hi!", 2) == "jk!"
-    assert Caesar.encrypt("9:00!", 2) == "9:00!"
+    assert Caesar.encrypt("9:00!", 2) == "1:22!"
+    assert Caesar.encrypt("xyz", 2) == "zab"
