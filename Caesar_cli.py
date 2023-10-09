6473c34a7513a7003d424b811ac6af988a7e91be
besele07 <besele07@rg16.at>
2023-10-09 15:57:20 +0200

    added brute force, decryption and error handling

--- Caesar_cli.py
+++ Caesar_cli.py
@@ -1,20 +1,38 @@
 import Caesar
 
-print("Willkommen zum besten Verschlüsselungsprogramm am BRG16!")
-print("="*57)
+print("Servus! Willkommen zum besten Verschlüsselungsprogramm am BRG16!")
+print("=" * 57)
+
+
+def get_key():
+    while True:
+        try:
+            key = int(input("Welches Passwort möchtest du verwenden?"))
+            break
+        except ValueError:
+            print("Hey du Dodl, du musst eine ganze Zahl eingeben!")
+    return key
 
 
 def do_encrypt():
     msg_clear = input("Was willst du verschlüsseln?")
-    key = int(input("Welches Passwort möchtest du verwenden?"))
+    key = get_key()
     msg_hidden = Caesar.encrypt(msg_clear, key)
     print(msg_hidden)
 
+
 def do_decrypt():
-    msg_clear = input("Was willst du entschlüsseln?")
-    key = int(input("Welches Passwort möchtest du verwenden?"))
-    msg_hidden = Caesar.decrypt(msg_clear, key)
-    print(msg_hidden)
+    msg_hidden = input("Was willst du entschlüsseln?")
+    key = get_key()
+    msg_clear = Caesar.decrypt(msg_hidden, key)
+    print(msg_clear)
+
+
+def do_bruteforce():
+    msg_hidden = input("Was willst du entschlüsseln?")
+    for key in range(26):
+        msg_clear = Caesar.decrypt(msg_hidden, key)
+        print(f"{key:02d}: {msg_clear}")
 
 
 while True:
@@ -23,7 +41,10 @@ while True:
         do_encrypt()
     elif selection == "d":
         do_decrypt()
+    elif selection == "b":
+        do_bruteforce()
+
     elif selection == "x":
         break
     else:
-        print("Funktion ({selection})konnte nicht gefunden werden")
+        print("Funktion ({selection}) konnte nicht gefunden werden")
